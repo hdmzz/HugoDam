02c69ec98f97c2923fbccc0e931bc435e19a20b9
hugo <hdamitzian@gmail.com>
2025-05-16 13:39:51 +0200

    modif carrousel

--- js/data.js
+++ js/data.js
@@ -102,51 +102,56 @@ const recentProjects = [
 // Données pour les projets en vitrine (pour le carrousel)
 const showcaseProjects = [
     {
-        id: "vieux_port",
-        name: "Site Vitrine E-commerce",
-        image: "reno-laithienne-QUgJhdY5Fyk-unsplash.jpg",
-        description: "Boutique en ligne avec paiement intégré",
-        technologies: "React, Node.js, MongoDB",
-        gridId: "carte_acti_vp"
+        id: "SunnyCi",
+        name: "SunnyCi",
+        description: "Localiser une terrasse au soleil !",
+        image: "Sunnycimapbox.png",
+        url: "https://hdmzz.github.io/sunnyCiMapbox/",
+        technologies: "JavaScript, MapBox, API géographiques",
+        gridId: "carte_acti_sunnyci"
     },
     {
-        id: "pomegues",
-        name: "Application de Gestion",
-        image: "paul-hermann-QFTrLdQIRhI-unsplash.jpg",
-        description: "Logiciel de gestion d'inventaire",
-        technologies: "Vue.js, Express, PostgreSQL",
-        gridId: "carte_acti_pomegues"
+        id: "ign",
+        name: "Visualisation 3D",
+        description: "Interface de visualisation de données géographiques",
+        image: "sunnyciign.png",
+        url: "https://hdmzz.github.io/DEMVis/",
+        technologies: "JavaScript 3D, WebGL, APIs cartographiques",
+        gridId: "carte_acti_ign"
     },
     {
-        id: "fioul",
-        name: "Progressive Web App",
-        image: "kevin-hikari-rV_Qd1l-VXg-unsplash.jpg",
-        description: "Application web avec fonctionnalités offline",
-        technologies: "React, Redux, Firebase",
-        gridId: "carte_acti_frioul"
+        id: "toralizer",
+        name: "Toralizer",
+        description: "Une commande permettant de faire passer n'importe quelle commande Linux par le réseau TOR.",
+        image: "Tor-logo-2011-flat.svg.png",
+        gitUrl: "https://github.com/hdmzz/toralizer.git",
+        technologies: "Linux, TOR, Shell",
+        gridId: "carte_acti_toralizer"
     },
     {
-        id: "calanques",
-        name: "Dashboard Analytique",
-        image: "kilyan-sockalingum-NR8-cBCN3aI-unsplash.jpg",
-        description: "Interface d'analyse de données avec graphiques",
-        technologies: "Angular, D3.js, Node.js",
-        gridId: "carte_acti_calanques"
+        id: "honey",
+        name: "Honey Pot",
+        description: "Honey Pot ecrit en python WIP",
+        image: "honey.png",
+        gitUrl: "https://github.com/hdmzz/honeypot.git",
+        technologies: "Python, Cybersécurité",
+        gridId: "carte_acti_honey"
     },
     {
-        id: "lagarde",
-        name: "API RESTful",
-        image: "florian-wehde-xW9e8gdotxI-unsplash.jpg",
-        description: "Backend sécurisé avec authentification JWT",
-        technologies: "Express, MongoDB, Passport.js",
-        gridId: "carte_acti_lagarde"
+        id: "ray",
+        name: "Raytracer C++",
+        description: "Un simple raytracer ecrit en C++",
+        image: "raytracer.jpg",
+        gitUrl: "https://github.com/hdmzz/raytracer.git",
+        technologies: "C++, Computer Graphics",
+        gridId: "carte_acti_ray"
     },
     {
-        id: "longchamp",
-        name: "Application Mobile",
-        image: "lena-paulin-wH2-EJoDcV0-unsplash.jpg",
-        description: "App cross-platform pour iOS et Android",
-        technologies: "React Native, Redux, Firebase",
-        gridId: "carte_acti_longchamp"
+        id: "Réseaux de neurones",
+        name: "Réseaux de neurones",
+        description: "Réseaux de neurones en C",
+        image: "nn_sbg.jpeg",
+        technologies: "C, Machine Learning, Neural Networks",
+        gridId: "carte_acti_nn"
     }
 ];
